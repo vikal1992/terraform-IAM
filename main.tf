d9b567c52248fddea2ddc9a41b5d1c9fbd5ac9ca
vikash <vikal.vikash18@gmail.com>
2025-08-16 17:25:17 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 resource "aws_iam_user" "IMA-user" {
   name = "terraform-user"
 
-
-
   tags = {
     tag-key = "terraform IAM user"
   }
+}
+
+resource "aws_iam_user_login_profile" "example" {
+  user    = "terreform-user"
 }
